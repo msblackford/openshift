f699f18007a85885bf99be2cfb8e0d98ccd2428e
Schweizer <mschweizer@deloitte.com.au>
2017-11-09 15:33:10 +1100

    blah

--- Consumer.py
+++ Consumer.py
@@ -7,17 +7,17 @@ from kafka import TopicPartition
 class Consumer:
     
     def run(self):
-        topics = 'ctm-transaction-topic'
+        topic = 'ctm-transaction-topic'
         group_id = 'prodcon_contest_2'
         servers = os.environ['SERVER'].split(',')
 
-        print( "Consuming Kafka messages on \n group id: " + str(group_id) + "\n topic: " + str(topics) + "\n servers: " + str(servers) )
+        print( "Consuming Kafka messages on \n group id: " + str(group_id) + "\n topic: " + str(topic) + "\n servers: " + str(servers) )
 
-        #partition = TopicPartition(topics, 0)
+        #partition = TopicPartition(topic, 0)
         #print("Partition: " + str(partition))
 
         consumer = KafkaConsumer(
-            topics,
+            topic,
             group_id=group_id,
             bootstrap_servers=servers,
             auto_offset_reset='earliest'
@@ -25,8 +25,7 @@ class Consumer:
 
         #consumer.assign( partition )
         #consumer.seek_to_beginning(partition)
-        
-        consumer.subscribe()
+        consumer.subscribe(topic)
 
         counter = 1
         for message in consumer:
