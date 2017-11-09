28e8bb450da6d5f1abb80953f99738136aff8ddd
Blackford <rblackford@deloitte.com.au>
2017-11-09 12:12:49 +1100

    printed output

--- Producer.py
+++ Producer.py
@@ -3,22 +3,25 @@ import os
 
 from kafka import KafkaProducer
 
-
 class Producer(threading.Thread):
 
     def run(self):
+        print('Starting Producer')
         producer = KafkaProducer(bootstrap_servers=os.environ['SERVER'].split(','))
-
         while True:
             producer.send('my-topic', b"Hello World!")
+            print('Produced to topic')
             time.sleep(1)
 
         producer.close()
 
 
+
 def main():
   produce = Producer()
   produce.run()
+
+
 
 
 if __name__ == "__main__":
