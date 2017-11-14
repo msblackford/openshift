f3639642e68dbfa1a8afb83dbd94bdc8b746fa58
marcusschweizer <marcus.schweizer@gmail.com>
2017-11-14 14:31:33 +1100

    Run from last 5 records in topic

--- Consumer.py
+++ Consumer.py
@@ -1,34 +1,64 @@
 import threading, logging, time
-import os
+import os, datetime
 
 from kafka import KafkaConsumer
 from kafka import TopicPartition
 
+
+def print_kafka_record(record):
+    return ("Received message: partition: {} | offset: {} | timestamp: {} | key: {} | value: {} ".format(
+                record.partition, 
+                record.offset, 
+                datetime.datetime.fromtimestamp(record.timestamp/1000).strftime('%Y-%m-%d %H:%M:%S.') + str(record.timestamp%1000),
+                record.key, 
+                record.value))
+
 class Consumer:
     
     def run(self):
-        topic = 'my-topic' # 'ctm-transactions-topic'
-        group_id = ''
-        #servers = os.environ['SERVER'].split(',')
-        servers = '192.168.88.129:9092'
+        # Run a continously consuming KafkaConsumer starting from last offset of last record
+        # must define partition
+        # 
+        # consumer = Consumer()
+        # producer.run_seek_by_offset()
 
-        print( "Consuming Kafka messages on \n group id: " + str(group_id) + "\n topic: " + str(topic) + "\n servers: " + str(servers) )
 
+        # setting up server variables
+        topic = 'ctm-transactions-topic'
+        partition = 0
+        group_id = None       
+        servers = os.environ['SERVER'].split(',') #Kafka broker server system environment variable $SERVER
+
+        # creating prodcuer instance
+        print('Starting Producer on: \n Server: {}\n Group ID: {}\n Topic: {}'.format(str(servers), str(group_id), str(topic)))
+
+        # if seeking, don't assign topic or auto_offset_reset here
         consumer = KafkaConsumer(
-            topic,
-            bootstrap_servers=servers,
-            auto_offset_reset='earliest' #this reads from start off queue
+            group_id=group_id,
+            bootstrap_servers=servers
             )
 
 
-        counter = 1
-        for message in consumer:
-            print(str(counter) + " Received message " + str(message.value) + " on topic '" + str(message.topic) + "'")
-            counter = counter + 1
+        topic_par = TopicPartition(topic, partition)
+
+        consumer.assign([topic_par])
+        current_pos = consumer.position(topic_par)
+        new_pos = current_pos - 5
+        if (new_pos < 0):
+            new_pos = 0
+        
+        consumer.seek(topic_par, new_pos ) # consumer starting at offset from end of queue
+
+        for record in consumer:
+            #continuously runs and waits for new record, code in here will run on each record received
+
+            print(print_kafka_record(record))
+
+            # process record
             
         print("Closing Kafka consumer")
         consumer.close()
-        
+            
         
 def main():
     con = Consumer()
